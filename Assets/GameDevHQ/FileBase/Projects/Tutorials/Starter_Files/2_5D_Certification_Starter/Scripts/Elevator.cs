56a7577c5a783dd84e86516d00852c83972c6a14
Jim Gagliardi <jvgagliardi98@outlook.com>
2021-01-18 19:42:02 -0500

    Added Elevator with wait Time

---
+++ Assets/GameDevHQ/FileBase/Projects/Tutorials/Starter_Files/2_5D_Certification_Starter/Scripts/Elevator.cs
@@ -0,0 +1,58 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Elevator : MonoBehaviour
+{
+    [SerializeField] private float _waitTime = 5.0f;
+    [SerializeField] private float _spd = 5.0f;
+    [SerializeField] private Transform _targetA, _targetB;
+    private bool _goingDown = false;
+    private bool _callElevator = false;
+    private bool _sendElevator = false;
+
+    private void Start()
+    {
+        transform.position = _targetA.position;    
+    }
+
+    private void FixedUpdate()
+    {
+        if (_sendElevator)
+        {
+            if (_goingDown)
+            {
+                transform.position = Vector3.MoveTowards(transform.position, _targetB.position, _spd * Time.deltaTime);
+            }
+            else
+            {
+                transform.position = Vector3.MoveTowards(transform.position, _targetA.position, _spd * Time.deltaTime);
+            }
+            
+            if(transform.position == _targetA.position || transform.position == _targetB.position)
+            {
+                _sendElevator = false;
+            }
+        }
+    }
+    public void MoveElevator()
+    {
+        _goingDown = !_goingDown;
+        if (_callElevator)
+        {
+            StartCoroutine(ElevatorWait(_waitTime));
+        }
+    }
+
+    IEnumerator ElevatorWait(float waitTime)
+    {
+        yield return new WaitForSeconds(waitTime);
+        _sendElevator = true;
+        _callElevator = false;
+    }
+
+    public void ReadyElevator()
+    {
+        _callElevator = true;
+    }
+}
---
+++ Assets/GameDevHQ/FileBase/Projects/Tutorials/Starter_Files/2_5D_Certification_Starter/Scripts/ElevatorPanel.cs
@@ -0,0 +1,54 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ElevatorPanel : MonoBehaviour
+{
+    [SerializeField] private bool _enableElevator = false;
+    private Elevator _elevator;
+    private MeshRenderer _renderer;
+
+    private void Start()
+    {
+        _elevator = GetComponent<Elevator>();
+        _renderer = GameObject.Find("Elevator_Panel").GetComponent<MeshRenderer>();
+    }
+
+    private void Update()
+    {
+        if(_enableElevator && Input.GetKeyDown(KeyCode.E))
+        {            
+            if(_elevator != null)
+            {
+                _elevator.ReadyElevator();
+                _elevator.MoveElevator();
+            }
+            
+            if(_renderer != null)
+            {
+                _renderer.material.color = Color.green;
+            }
+        }
+        if (!_enableElevator)
+        {
+            _renderer.material.color = Color.red;
+        }
+    }
+
+    private void OnTriggerStay(Collider other)
+    {
+        if (other.tag == "Player")
+        {
+            Debug.Log("Collided with Player");
+            _enableElevator = true;
+        }
+    }
+
+    private void OnTriggerExit(Collider other)
+    {
+        if (other.tag == "Player")
+        {
+            _enableElevator = false;
+        }
+    }
+}
