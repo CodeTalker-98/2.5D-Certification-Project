e32f3cec5af312374ad9ef2dd05f3b29b91e048a
Jim Gagliardi <jvgagliardi98@outlook.com>
2021-01-19 19:47:59 -0500

    Made moving platform Prefab

---
+++ Assets/GameDevHQ/FileBase/Projects/Tutorials/Starter_Files/2_5D_Certification_Starter/Scripts/MovingPlatform.cs
@@ -0,0 +1,52 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MovingPlatform : MonoBehaviour
+{
+    [SerializeField] private float _spd = 5.0f;
+    [SerializeField] private Transform _targetA, _targetB;
+    private bool _switch = false;
+
+    private void Start()
+    {
+        transform.position = _targetA.position;
+    }
+
+    private void FixedUpdate()
+    {
+        if(transform.position == _targetA.position)
+        {
+            _switch = false;
+        }
+        if(transform.position == _targetB.position)
+        {
+            _switch = true;
+        }
+
+        if (_switch)
+        {
+            transform.position = Vector3.MoveTowards(transform.position, _targetA.position, _spd * Time.deltaTime);
+        }
+        else
+        {
+            transform.position = Vector3.MoveTowards(transform.position, _targetB.position, _spd * Time.deltaTime);
+        }
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.tag == "Player")
+        {
+            other.transform.parent = this.transform;
+        }
+    }
+
+    private void OnTriggerExit(Collider other)
+    {
+        if(other.tag == "Player")
+        {
+            other.transform.parent = null;
+        }
+    }
+}
