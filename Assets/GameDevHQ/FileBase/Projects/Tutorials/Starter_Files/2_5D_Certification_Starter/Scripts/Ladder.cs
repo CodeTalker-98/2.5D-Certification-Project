c1da28b472cc8aa331fc658d0f1eaccab4472507
Jim Gagliardi <jvgagliardi98@outlook.com>
2021-01-24 14:21:55 -0500

    Added Idle Jump

--- Assets/GameDevHQ/FileBase/Projects/Tutorials/Starter_Files/2_5D_Certification_Starter/Scripts/Ladder.cs
+++ Assets/GameDevHQ/FileBase/Projects/Tutorials/Starter_Files/2_5D_Certification_Starter/Scripts/Ladder.cs
@@ -5,4 +5,28 @@ using UnityEngine;
 public class Ladder : MonoBehaviour
 {
     [SerializeField] private Transform _standPos; //will use @ top of ladder
+    [SerializeField] private Transform _climbPos;
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.CompareTag("LadderChecker"))
+        {
+            Player player = other.transform.parent.GetComponent<Player>();
+
+            if(player != null)
+            {
+                player.LadderClimb(_climbPos, this);
+            }
+        }
+    }
+    /*
+    public Vector3 ClimbOffset()
+    {
+        return _climbPos.position;
+    }*/
+
+    public Vector3 StandOffset()
+    {
+        return _standPos.position;
+    }
 }
